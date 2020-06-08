365bfbf862f50e2f765006445c184a6692e3d885
jimersylee <jimersylee@gmail.com>
2020-06-08 12:57:38 +0800

    update

--- app/web.go
+++ app/web.go
@@ -108,8 +108,8 @@ func initRouter(app *iris.Application) {
 	})
 	app.Get("/metrics", iris.FromStd(promhttp.Handler()))
 
-	app.Any("/api/steamapi/{directory:path}", services.ProxyService.ProxyApi)
-	app.Any("/api/steamcommunity/{directory:path}", services.ProxyService.ProxyCommunity)
+	app.Any("/api/steamapi/{directory:path}", services.ProxyService.Proxy)
+	app.Any("/api/steamcommunity/{directory:path}", services.ProxyService.Proxy)
 
 	mvc.Configure(app.Party("/api"), func(application *mvc.Application) {
 		application.Party("/user").Handle(new(api.UserController))
--- services/proxy_service.go
+++ services/proxy_service.go
@@ -1,10 +1,10 @@
 package services
 
 import (
-	"fmt"
 	"github.com/jimersylee/iris-seed/commons"
 	"github.com/kataras/iris"
 	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 type ProxyInterface interface {
@@ -22,40 +22,31 @@ func NewProxyService() *ProxyServiceImpl {
 type ProxyServiceImpl struct {
 }
 
-func (p *ProxyServiceImpl) ProxyCommunity(ctx iris.Context) {
+func (p *ProxyServiceImpl) Proxy(ctx iris.Context) {
 	uri := ctx.Request().RequestURI
-	uriNeed := uri[len("/api/steamcommunity/") : len(uri)-1]
-	//todo：找出能用的ip
-	ipModel := IpService.FindOne(commons.NewSqlCnd().Where("status=1").Asc("request_times"))
-	logrus.Info("ipModel:", ipModel)
-	if ipModel == nil {
-		logrus.Error("can't find available ip")
-		ctx.ResponseWriter().WriteHeader(500)
-		return
-	}
-	ip := ipModel.Ip
-	webUrl := "http://steamcommunity.com/" + uriNeed
-	responseStr, statusCode := IpService.Proxy(ip, webUrl)
-	fmt.Println(responseStr)
-	ctx.ResponseWriter().WriteHeader(statusCode)
-	ctx.WriteString(responseStr)
-}
 
-func (p *ProxyServiceImpl) ProxyApi(ctx iris.Context) {
-	uri := ctx.Request().RequestURI
-	uriNeed := uri[len("/api/steamapi/") : len(uri)-1]
+	//判断是代理api还是社区
+	uriNeed := ""
+	webUrl := ""
+	if strings.Index(uri, "steamapi") > 0 {
+		uriNeed = uri[len("/api/steamapi/"):]
+		webUrl = "http://api.steampowered.com/" + uriNeed
+	} else {
+		uriNeed = uri[len("/api/steamcommunity/"):]
+		webUrl = "http://steamcommunity.com/" + uriNeed
+	}
 	//todo：找出能用的ip
 	ipModel := IpService.FindOne(commons.NewSqlCnd().Where("status=1").Asc("request_times"))
-	logrus.Info("ipModel:", ipModel)
+	logrus.Infof("ipModel:%s,uri:%s,uriNeed:%s,webUrl:%s", ipModel, uri, uriNeed, webUrl)
 	if ipModel == nil {
 		logrus.Error("can't find available ip")
 		ctx.ResponseWriter().WriteHeader(500)
 		return
 	}
 	ip := ipModel.Ip
-	webUrl := "http://api.steampowered.com/" + uriNeed
 	responseStr, statusCode := IpService.Proxy(ip, webUrl)
-	fmt.Println(responseStr)
+	logrus.Infof("steam返回数据：%s", responseStr)
 	ctx.ResponseWriter().WriteHeader(statusCode)
 	ctx.WriteString(responseStr)
+
 }
