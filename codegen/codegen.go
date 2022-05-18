34b16a989d74887ba557d62d733e1498b7daaf35
jimersylee <jimersylee@gmail.com>
2022-05-18 14:32:14 +0800

    comment

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -5,6 +5,9 @@ import (
 	"github.com/jimersylee/iris-seed/models"
 )
 
+/**
+ * 这是 iris-seed 代码生成器
+ */
 func main() {
 	code_generator.Generate("./", "github.com/jimersylee/iris-seed", code_generator.GetGenerateStruct(&models.Article{}))
 }
